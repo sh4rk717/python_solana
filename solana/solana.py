67b261a9757e9b10f287261527a72ebe0f4b6c65
sh4rk <qden84@gmail.com>
2022-04-04 02:13:11 +0300

    implemented option with vote info impl function+option validators info

--- solana/solana.py
+++ solana/solana.py
@@ -4,10 +4,18 @@ from datetime import datetime
 from datetime import timedelta
 
 
+def menu():
+    print('Check balance - 1')
+    print('Leaderslots   - 2')
+    print('Epoch info    - 3')
+    print('Vote info     - 4')
+    print('Validators    - 5')
+
+
 def get_vote(identity_list):
     vote_list = []
-    for key in range(len(identity_list)):
-        vote = subprocess.check_output('solana validators | grep ' + identity_list[key] + ' | awk \'{print $3}\'',
+    for i in range(len(identity_list)):
+        vote = subprocess.check_output('solana validators | grep ' + identity_list[i] + ' | awk \'{print $3}\'',
                                        shell=True, universal_newlines=True)
         vote_list.append(vote[:-1])
     return vote_list
@@ -43,29 +51,31 @@ def end_of_epoch():
 
 
 def check_balance(identity_list, vote_list):
-    for key in range(len(identity_list)):
-        bal1 = subprocess.check_output('solana balance ' + identity_list[key], universal_newlines=True, shell=True)
-        bal2 = subprocess.check_output('solana balance ' + vote_list[key], universal_newlines=True,
+    for i in range(len(identity_list)):
+        bal1 = subprocess.check_output('solana balance ' + identity_list[i], universal_newlines=True, shell=True)
+        bal2 = subprocess.check_output('solana balance ' + vote_list[i], universal_newlines=True,
                                        shell=True)
-        print('Identity (' + identity_list[key][:5] + '):\t' + bal1, end='')
-        print('Vote (' + vote_list[key][:5] + '):\t\t' + bal2)
+        print('Identity (' + identity_list[i][:5] + '):\t' + bal1, end='')
+        print('Vote (' + vote_list[i][:5] + '):\t\t' + bal2)
 
 
 def leader_slots(identity_list):
-    for key in range(len(identity_list)):
-        validator_info = subprocess.check_output('solana validators | grep ' + identity_list[key],
+    for i in range(len(identity_list)):
+        validator_info = subprocess.check_output('solana validators | grep ' + identity_list[i],
                                                  universal_newlines=True, shell=True)
-        total = subprocess.check_output('solana leader-schedule | grep ' + identity_list[key] + ' | wc -l',
+        total = subprocess.check_output('solana leader-schedule | grep ' + identity_list[i] + ' | wc -l',
                                         universal_newlines=True, shell=True)
+        # Added for validators with no "Completed Leader Slots" by now
         try:
-            leader_slots_info = subprocess.check_output('solana block-production | grep ' + identity_list[key],
+            leader_slots_info = subprocess.check_output('solana block-production | grep ' + identity_list[i],
                                                         universal_newlines=True, shell=True)
         except subprocess.CalledProcessError as e:
             leader_slots_info = '0' * 81
+
         credits = int(validator_info[143:150])
         blocks_produced = int(leader_slots_info[75:82])
         commission = credits * 5e-6 - blocks_produced * 375e-5
-        print(identity_list[key][:5])
+        print(identity_list[i][:5])
         print(
             'Total leader slots   Completed Leader Slots  Blocks Produced    Skipped Slots  Skipped Slot Percentage   Commission')
         if leader_slots_info[0] != '0':
@@ -75,15 +85,21 @@ def leader_slots(identity_list):
             print(total[:-1].rjust(18) + ' ' * 84 + '◎{0:.3f}'.format(commission).rjust(13) + '\n')
 
 
-print('Check balance - 1')
-print('Leaderslots   - 2')
-print('Epoch info    - 3')
+def validators(identity_list):
+    ids = ''
+    for i in identity_list:
+        ids = ids + ' -e ' + i
+    subprocess.Popen('solana validators --sort=credits -r -n | sed -n 1,2p && solana validators --sort=credits -r -n | grep' + ids, shell=True).wait()
 
+
+##########################################################################################################
 identity = ['A1enabzLW77R2VVg67CLv3kNJ5FWVnAmC6pKcZwCmkXB', 'sh4rkGLyKwi8q1n8bwYkDiUC2n1cs6tbWSTGogd45d6']
 vote = get_vote(identity)
 
+menu()
+
 while True:
-    cmd = input('Choose option: ')
+    cmd = input('\nChoose option: ')
     if cmd == '1':
         print('*** Balances ***')
         check_balance(identity, vote)
@@ -94,5 +110,20 @@ while True:
         print('*** Epoch Info ***')
         subprocess.Popen('solana epoch-info', shell=True).wait()
         end_of_epoch()
+    elif cmd == '4':
+        print('*** Vote Info with Rewards ***')
+        for key in vote:
+            while True:
+                n = input('How many reward epochs to show for vote [' + key + ']? Enter number between 1 and 5: ')
+                if n in ['1', '2', '3', '4', '5']:
+                    break
+                else:
+                    print('Try again...')
+            subprocess.Popen('solana vote-account ' + key + ' --with-rewards --num-rewards-epochs ' + n,
+                             shell=True).wait()
+        menu()
+    elif cmd == '5':
+        print('*** Validators info ***')
+        validators(identity)
     else:
         break
